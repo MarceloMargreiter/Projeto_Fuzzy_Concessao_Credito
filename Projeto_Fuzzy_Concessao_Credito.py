3dc74a067c846a62aef8bc000bc11221a32c4ed3
MarceloMargreiter <marcelo.margreiter@gmail.com>
2026-05-21 22:46:49 -0300

    ommit_2 21/02/2026

--- Projeto_Fuzzy_Concessao_Credito.py
+++ Projeto_Fuzzy_Concessao_Credito.py
@@ -23,11 +23,11 @@ renda_mensal['media'] = fuzz.trapmf(renda_mensal.universe, [3000, 6000, 10000, 2
 renda_mensal['alta'] = fuzz.trapmf(renda_mensal.universe, [10000, 20000, 100000, 100000])     
 
 #Funções de pertinência para limite de crédito (será o % da renda mensal)
-limite_credito['muito_baixo'] = fuzz.trimf(limite_credito.universe, [0, 0, 25]) # limite inferior um pouco maior que a média.
-limite_credito['baixo'] = fuzz.trimf(limite_credito.universe, [15, 40, 50])
-limite_credito['medio'] = fuzz.trimf(limite_credito.universe, [40, 50, 85])
-limite_credito['alto'] = fuzz.trimf(limite_credito.universe, [50, 85, 100])
-limite_credito['muito_alto'] = fuzz.trimf(limite_credito.universe, [85, 100, 100]) # limite superior um pouco menor que a média.    
+limite_credito['muito_baixo'] = fuzz.trimf(limite_credito.universe, [0, 12, 24]) 
+limite_credito['baixo'] = fuzz.trimf(limite_credito.universe, [12, 24, 36])
+limite_credito['medio'] = fuzz.trimf(limite_credito.universe, [24, 36, 48])
+limite_credito['alto'] = fuzz.trimf(limite_credito.universe, [36, 48, 70])
+limite_credito['muito_alto'] = fuzz.trimf(limite_credito.universe, [48, 70, 70]) # limite superior um pouco menor para näo comprometer mais que 70% da renda mensal.    
 
 #Regras Fuzzy
 regra1 = ctrl.Rule(score_serasa['baixo'] & renda_mensal['baixa'], limite_credito['muito_baixo'])
@@ -48,16 +48,31 @@ sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3, regra4, regra5, r
 #Simulação
 simulacao = ctrl.ControlSystemSimulation(sistema_controle)  
 
-#Entrada de dados
-Renda_Informada = 7000
-Score_Informado = 100
-simulacao.input['Score'] = Score_Informado   
-simulacao.input['Renda'] = Renda_Informada
+# Entrada de dados
+Renda_Informada = float(input("\nDigite a renda mensal (R$): "))
 
+# Loop para validar o Score
+while True:
+    Score_Informado = float(input("Digite o Score Serasa (0 a 100 pts): "))
+    if 0 <= Score_Informado <= 100:
+        # Passando para o sistema fuzzy somente quando válido
+        simulacao.input['Score'] = Score_Informado
+        simulacao.input['Renda'] = Renda_Informada
+        break
+    else:
+        print("Erro: O Score deve estar entre 0 e 100 pontos. Tente novamente.")
 
-#Cálculo do resultado
+# Cálculo do resultado
 simulacao.compute()
-print(f"\nRenda: R${Renda_Informada:.0f} \nLimite de Crédito (%): {simulacao.output['Credito']:.2f}% da renda mensal \nLimite de Crédito Liberado (R$): R$ {round((simulacao.output['Credito'] / 100) * Renda_Informada, -2):.0f}\n")
+
+# Saída formatada
+limite_percentual = simulacao.output['Credito']
+limite_reais = round((limite_percentual / 100) * Renda_Informada, -2)
+
+print(f"\nRenda: R${Renda_Informada:.0f} "
+      f"\nScore Serasa: {Score_Informado:.0f} pts "
+      f"\nLimite de Crédito (%): {limite_percentual:.2f}% da renda mensal "
+      f"\nLimite de Crédito Liberado (R$): R$ {limite_reais:.0f}\n")
 
 # #Visualização dos gráficos de pertinência
 # score_serasa.view(simulacao)
